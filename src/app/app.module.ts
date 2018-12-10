eeba2938e8c1dce8df55389960ab0140088f119b
D-ries <swinnendries@gmail.com>
2018-12-10 14:08:24 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { MovieService } from './services/movie.service';
+import { MovieItemComponent } from './components/movie-item/movie-item.component';
+import { AddMovieComponent } from './components/add-movie/add-movie.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieItemComponent,
+    AddMovieComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
